9f198cefbeabee054da41f5bd0211e538920c5ee
Pavel <doc_petrovich@mail.ru>
2022-12-12 16:07:02 +0300

    add Tasks

---
+++ seminar1/hw task 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7, min = 5
+// a = 2 b = 10 -> max = 10, min = 2
+// a = -9 b = -3 -> max = -3, min = -9
+
+Console.WriteLine("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB) Console.WriteLine("Числа равны.");
+else
+{
+    if (numberA > numberB) Console.WriteLine("Первое число больше второго.");
+    else Console.WriteLine("Второе число больше первого");
+}
---
+++ seminar1/hw task 4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (numberA >= numberB && numberA >= numberC) max = numberA;
+if (numberB >= numberA && numberB >= numberC) max = numberB;
+if (numberC >= numberA && numberC >= numberB) max = numberC;
+
+Console.WriteLine($"Максимальное число - {max}");
